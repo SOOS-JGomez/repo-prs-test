ba11dd6b18c896958da5f590cf6af8e929007fa6
SOOS-JGomez <106080138+SOOS-JGomez@users.noreply.github.com>
2022-09-23 17:00:46 -0300

    Add files via upload

--- mix/mix.exs
+++ mix/mix.exs
@@ -1,67 +1,40 @@
-defmodule UeberauthTwitter.Mixfile do
+defmodule Keshikimi2.MixProject do
   use Mix.Project
-
-  @source_url "https://github.com/ueberauth/ueberauth_twitter"
-  @version "0.4.1"
 
   def project do
     [
-      app: :ueberauth_twitter,
-      version: @version,
-      name: "Ueberauth Twitter Strategy",
-      package: package(),
-      elixir: "~> 1.1",
-      build_embedded: Mix.env() == :prod,
+      app: :keshikimi2,
+      version: "0.1.0",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      source_url: @source_url,
-      homepage_url: @source_url,
       deps: deps(),
-      docs: docs()
+      aliases: aliases()
     ]
   end
 
   def application do
-    [applications: [:logger, :httpoison, :oauther, :ueberauth]]
+    [
+      extra_applications: [:logger],
+      mod: {Keshikimi2.Application, [Default]}
+    ]
   end
 
   defp deps do
     [
-      {:httpoison, "~> 1.0"},
-      {:oauther, "~> 1.1"},
-      {:ueberauth, "~> 0.7"},
-
-      # dev/test dependencies
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:ex_doc, "~> 0.18", only: :dev},
-      {:credo, "~> 0.8", only: [:dev, :test]}
+      {:cachex, "~> 3.1"},
+      {:deps_ghq_get, "~> 0.1.2", only: :dev},
+      {:floki, "~> 0.20.0"},
+      {:httpoison, "~> 1.4"},
+      {:httpoison_form_data, "~> 0.1.3"},
+      {:poison, "~> 3.1"},
+      {:timex, "~> 3.1"},
+      {:yaml_elixir, "~> 2.1.0"}
     ]
   end
 
-  defp docs do
+  defp aliases do
     [
-      extras: [
-        "CHANGELOG.md": [],
-        "CONTRIBUTING.md": [title: "Contributing"],
-        "LICENSE.md": [title: "License"],
-        "README.md": [title: "Overview"]
-      ],
-      main: "readme",
-      source_url: @source_url,
-      source_ref: "v#{@version}",
-      formatters: ["html"]
-    ]
-  end
-
-  defp package do
-    [
-      description: "An Uberauth strategy for Twitter authentication.",
-      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
-      maintainers: ["Sean Callan"],
-      licenses: ["MIT"],
-      links: %{
-        Changelog: "https://hexdocs.pm/ueberauth_twitter/changelog.html",
-        GitHub: @source_url
-      }
+      "deps.get": ["deps.get", "deps.ghq_get --async"]
     ]
   end
 end
